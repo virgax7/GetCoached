245eb824d70335f3999e5527a5aeab8a500265e0
pyoung <pyoungx7@gmail.com>
2018-06-27 20:39:04 -0400

    initial accountservice bootstrap

---
+++ account-service/src/test/java/com/getcoached/account/AccountServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package com.getcoached.account;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccountServiceApplicationTest {
+
+    @Test
+    public void applicationContextLoads() {
+        // makes sure bootstrapping for the Spring application context works and no exception is thrown
+    }
+}
